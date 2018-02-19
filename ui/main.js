26ead59e756733c64cee8db579cb3a161c3a6def
ronyjoe98 <36446259+ronyjoe98@users.noreply.github.com>
2018-02-19 09:34:41 -0600

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='new text';
+var img=document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft= '100px';
+}
