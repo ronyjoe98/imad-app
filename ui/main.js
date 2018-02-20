fab020de9902d11adaa135c5bb5d8f32df5599e8
ronyjoe98 <36446259+ronyjoe98@users.noreply.github.com>
2018-02-20 11:42:20 -0600

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 var button=document.getElementById('counter');
 button.onclick = function(){
-    counter+=1;
+    counter = counter + 1;
     var span=document.getElementById('count');
     span.innerHTML = counter.toString();
 }
