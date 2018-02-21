3f5e16e8c528c033e61e3b563b8e51d00d1bdf83
ronyjoe98 <36446259+ronyjoe98@users.noreply.github.com>
2018-02-21 01:54:43 -0600

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,8 +16,7 @@ button.onclick = function(){
     request.open('GET', 'http://ronyjoe98.imad.hasura-app.io/counter',true);
     request.send(null);
 };
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
       var request = new XMLHttpRequest();
@@ -36,7 +35,9 @@ submit.onclick = function(){
                    ul.innerHTML=list;
             }
         }
-    }; 
+    };
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://ronyjoe98.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
   
