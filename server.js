66981672b5c5759d872cb1cb992e3a9121aa5bf5
ronyjoe98 <36446259+ronyjoe98@users.noreply.github.com>
2018-02-25 10:29:37 -0600

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,8 +92,8 @@ app.get('/submit-name',function(req,res){
 });
 
 app.get('/articles/:articleName',function(req,res){
-   var articleName = req.params.articleName;
-   pool.query("SELECT * FROM article WHERE TITLE='" + articleName + "'",function(err,result){
+  
+   pool.query("SELECT * FROM article WHERE TITLE='" + req.params.articleName + "'",function(err,result){
        if(err)
        {
             res.status(500).send(err.toString());
