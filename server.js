7975968454e815068a5cb85821310234c831b357
ronyjoe98 <36446259+ronyjoe98@users.noreply.github.com>
2018-03-10 20:33:07 -0600

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto=require('crypto');
 var config = {
     user: 'ronyjoe98',
     databse: 'ronyjoe98',
@@ -113,7 +114,15 @@ app.get('/articles/:articleName',function(req,res){
    });
    
 });
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed;
+}
 
+app.get('/hash/:input',function(req,res){
+    var hashedString = hash(req.params.input,'this-is-ma=string');
+    res.send(hashedString);
+})
 app.get('/article-two',function(req,res){
     res.send("Article two requested and will be served here");
 });
